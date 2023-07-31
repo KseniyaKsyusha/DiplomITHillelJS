16aac99c57e93ffc6381add41bb6616421c2e14d
KseniyaKsyusha <109752337+KseniyaKsyusha@users.noreply.github.com>
2023-07-31 18:16:46 +0300

    Add files via upload

--- my-project/src/containers/Login/index.js
+++ my-project/src/containers/Login/index.js
@@ -1,1 +1,17 @@
+import './style.css';
+import LoginForm from '../../components/LoginForm';
+import logo from "../../assets/logo.png"
 
+const Login = () => {
+
+    return (
+        <div className="containerLogin">
+            <div className="containerLogo">
+                <img src={logo} className="loginLogo" alt="Rozetka logo"/>
+            </div>
+            <LoginForm />
+        </div>
+    )
+}
+
+export default Login;
