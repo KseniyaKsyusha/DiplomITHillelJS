b9f83f25f7f9e4a17ca7ae5cd0b8748f06aac8d2
KseniyaKsyusha <109752337+KseniyaKsyusha@users.noreply.github.com>
2023-08-01 12:55:26 +0300

    Add files via upload

--- my-project/src/components/Button/index.js
+++ my-project/src/components/Button/index.js
@@ -1,1 +1,15 @@
+import './style.css';
 
+
+const Button = () => {
+ 
+  return (
+    <form className='loginForm'>
+               
+      <button type="submit" className="loginButton">Login</button>
+
+    </form>
+  );
+}
+
+export default Button;
